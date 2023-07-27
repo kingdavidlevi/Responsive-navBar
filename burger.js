84974060b4e9986d3d56c02b8514508e420cc760
kingdavidlevi <levikingdavid4040@gmail.com>
2023-07-27 15:45:50 -0700

    first commit

---
+++ burger.js
@@ -0,0 +1,15 @@
+
+        const burger = document.querySelector('.burger');
+        const nav = document.querySelector('.ul-links');
+
+        burger.addEventListener('click' , () =>  {
+            nav.classList.toggle('ul-active');
+
+     });
+
+    
+   
+    
+
+
+
